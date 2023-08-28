83e51e0f6581c83c54067c97ff03610669513bca
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2023-08-28 18:03:27 -0300

    versao 1.2

--- src/componentes/layout/Cards.js
+++ src/componentes/layout/Cards.js
@@ -7,9 +7,13 @@ import styles from './Cards.module.css'
 
 function Cards({img,periodo,escola,idTabela, eventPai}){
     const [textPeriodo, setTextPeriodo] = useState(periodo)
+    const [ textoEscola, setTextoEscola] = useState(escola)
     function mudarTextoPeriodo(e) {
         setTextPeriodo(e.target.value)
       }
+    function mudarTextoEscola(e){
+        setTextoEscola(e.target.value)
+    }
     
     
     return(
@@ -17,7 +21,8 @@ function Cards({img,periodo,escola,idTabela, eventPai}){
         <div><img src={img}/></div>
             <div>
                 {eventPai?<input type='text' value={textPeriodo} onChange={mudarTextoPeriodo}  />:  <h2>{textPeriodo}</h2> }
-                {eventPai?<input/>:  <h3>{escola}</h3>}
+                {eventPai?<input type='text' value={textoEscola} onChange={mudarTextoEscola}  />:  <h3>{textoEscola}</h3> }
+             
               
               
                 <LinkButton text="Entra" to="/tabela"/> 
--- src/componentes/pages/Gerenciar.js
+++ src/componentes/pages/Gerenciar.js
@@ -4,17 +4,21 @@ import {LuChevronLeft} from "react-icons/lu"
 
 import Container from "../layout/Container"
 import styles from "./Gerenciar.module.css" 
-import LinkButton from '../layout/LinkButton'
+
 import Cards from "../layout/Cards"
 
 import creche from "../../imgs/creche.jpg"
 import parquinho from "../../imgs/parquinho.jpg"
 import professora_sala_aula from "../../imgs/professora_sala_aula.jpg"
 import ensino_medio from "../../imgs/ensino_medio.jpg"
+
+import icon_lapis from "../../imgs/icons/lapis.png"
+import icon_papel from "../../imgs/icons/folha-de-papel.png"
+
 function Gerenciar(){
     const [editou, setEditou] = useState(false)
     var clicou = false
-    function click(){
+    function editar(){
         if(!clicou){
             clicou= true
        
@@ -26,31 +30,46 @@ function Gerenciar(){
     }
     return(
         <div className={styles.containerCards}>
-            <h1 className={styles.introducao}>Gerenciamento</h1>
+            <div className={styles.superior_ger}>
+
+                <h1 className={styles.introducao}>Gerenciamento</h1>
+                <div className={styles.icons_ger}>
+                    <div className={styles.icon_ger}>
+                        <img src={icon_lapis} onClick={()=> editar() } />
+                    </div>
+                    <div className={styles.icon_ger}>
+                        <img src={icon_papel}  />
+                    </div>
+                    
+                   
+                </div>
+            </div>
+            
            <Container>
                
                 <Cards img={creche} periodo="Manhã" escola="Creche" eventPai={editou}/>
-                <Cards img={parquinho} periodo="Manhã" escola="Parquinho"/>
-                <Cards img={ensino_medio} periodo="Manhã" escola="Escola"/>
-                <Cards img={professora_sala_aula} periodo="Tarde" escola="Escola"/>
+                <Cards img={parquinho} periodo="Manhã" escola="Parquinho" eventPai={editou}/>
+                <Cards img={ensino_medio} periodo="Manhã" escola="Escola" eventPai={editou}/>
+                <Cards img={professora_sala_aula} periodo="Tarde" escola="Escola" eventPai={editou}/>
                 
                
                
                 
             </Container> 
-            <div className={styles.menu_lateral}>
+            
+            {/* <div className={styles.menu_lateral}>
                 <input type="checkbox" id="check_menu" className={styles.inputMenu}/>
                 <label for="check_menu">
                     <LuChevronLeft/>
 
                 </label>
                 <ul className={styles.opcao_menu_lateral}>
-                    <li onClick={()=> click() }>Editar</li>
+                    <li onClick={()=> editar() }>Editar</li>
                     <li>Criar</li>
                     <li>Excluir</li>
                   
                 </ul>
-            </div>
+            </div> */}
         </div>
     )
 }
