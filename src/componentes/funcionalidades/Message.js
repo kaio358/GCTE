2e589c02de6ba3d3ee2661adeb79f89a06147774
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2024-01-08 17:16:21 -0300

    1.7

--- src/componentes/funcionalidades/Message.js
+++ src/componentes/funcionalidades/Message.js
@@ -3,11 +3,11 @@ function Message(props) {
     return(
         <li className={styles.message_c}>
             <div className={styles.foto_message}>
-                <img src=""/>
+                <img src="https://placeholder.com/64"/>
             </div>
             <div className={styles.conteudo_message}>
-                <span>Nome</span>
-                <span>Pagou</span>
+                <span>Kaio Luiz Ferreira</span>
+                <span className={styles.importante_message}>{props.mensagem_pago_ou_nao}</span>
             </div>
             <div className={ ` ${styles.classificacao_message} ${styles[props.customCor]}` }>
 
--- src/componentes/pages/CaixaDeMensagem.js
+++ src/componentes/pages/CaixaDeMensagem.js
@@ -1,20 +1,24 @@
+import { useState } from "react"
 import Message from "../funcionalidades/Message"
 import styles from "./CaixaDeMensagem.module.css"
 function CaixaDeMensagem() {
+    const [teste , setTeste] = useState()
     return(
-        <div>
+        <div className={styles.div_caixa_mensagem}>
+
             <section>
-                <ul>
+                <ul className={styles.guia_caixa_de_mensagem}>
                     <li>Geral</li>
                     <li>Importante</li>
+                    <li>Lido(s)</li>
                 </ul>
-                <div>
-                    lido
-                </div>
+               
             </section>
             <section className={styles.box_message}>
                 <ul>
-                    <Message customCor={"corVer"}/>
+                    <Message customCor={"corVer"} mensagem_pago_ou_nao={"Não pagou"}/>
+                    <Message customCor={"corGreen"} mensagem_pago_ou_nao={"Pagou"}/>
+                    <Message customCor={"corGreen"} mensagem_pago_ou_nao={"Pagou"}/>
                 </ul>
             </section>
         </div>
