13d1f4c4f92cd340e6ba700c5c1098bb5e62a937
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2024-11-27 18:09:34 -0300

    essa breanch somente o normal

--- back_end/index_servidor.js
+++ back_end/index_servidor.js
@@ -1,58 +1,72 @@
 const express = require("express");
+const http = require("http");  
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
+
+const app = express();
+const server = http.createServer(app); 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+app.use(express.json());
+
+const conexao = require("./infraestrutura/conexao")
+const Tabelas = require("./infraestrutura/tabelas")
+
+const rotaEscola = require("./rotas/rotaEscola")
+const rotaImagens = require("./rotas/rotaImagens")
+const rotaPessoa = require("./rotas/rotaPessoa")
+const rotaPagamento = require("./rotas/rotaPagamento")
+const rotaMensagem = require("./rotas/rotaMensagem")
+const rotaTransacao = require("./rotas/rotaTransacao")
+
+
+
+// para conferir pagamento diario
 const cron = require('node-cron');
-const conexao = require("./infraestrutura/conexao");
-const Tabelas = require("./infraestrutura/tabelas");
-
-const rotaEscola = require("./rotas/rotaEscola");
-const rotaImagens = require("./rotas/rotaImagens");
-const rotaPessoa = require("./rotas/rotaPessoa");
-const rotaPagamento = require("./rotas/rotaPagamento");
-const rotaMensagem = require("./rotas/rotaMensagem");
-const rotaTransacao = require("./rotas/rotaTransacao");
-
 const Pagamento = require("./modelos/Pagamento");
 const Mensagem = require("./modelos/Mensagem");
 
-const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.json());
 
-// Configurar rotas
-app.use("/", rotaEscola);
-app.use("/", rotaImagens);
-app.use("/", rotaPessoa);
-app.use("/", rotaPagamento);
-app.use("/", rotaMensagem);
-app.use("/", rotaTransacao);
+conexao.connect(erro=>{
+    if(erro){
+        console.log(erro);
+    }else{
+        
 
-// Conexão com banco e inicialização de tabelas
-conexao.connect(erro => {
-    if (erro) {
-        console.log("Erro na conexão com o banco:", erro);
-    } else {
-        console.log("Conexão com o banco de dados bem-sucedida.");
-        Tabelas.init(conexao);
+        Tabelas.init(conexao)
 
-        // Agendar cron jobs (atenção: Lambda não suporta cron diretamente)
         cron.schedule('0 0 * * *', async () => {
             try {
                 const today = new Date();
-                await Pagamento.atualizarDevedor(today);
-                const pagamentosNaoPagos = await Pagamento.pegarPorDataENaoPagou();
-                pagamentosNaoPagos.forEach(pnp => {
-                    Mensagem.atualizaOuInseriImportante(pnp.idPagamento);
-                });
-                console.log("Verificação diária de pagamentos concluída.");
+                Pagamento.atualizarDevedor(today)
+                const pegarPorDataENaoPagou = await Pagamento.pegarPorDataENaoPagou();
+                pegarPorDataENaoPagou.map(pnp=>{
+              
+                    const mensagem =  Mensagem.atualizaOuInseriImportante(pnp.idPagamento)
+                   
+                })
+               
+
+                // console.log("ola");
             } catch (error) {
                 console.error('Erro ao verificar pagamentos:', error);
             }
         });
-    }
-});
+        
+        app.use("/",rotaEscola)
+        app.use("/",rotaImagens)
+        app.use("/", rotaPessoa)
+        app.use("/",rotaPagamento)
+        app.use("/",rotaMensagem)
+        app.use("/",rotaTransacao)
+        server.listen(5000, () => {
+            console.log("Conectado: http://localhost:5000");
+        });
 
-module.exports = app; 
+    }
+})
--- back_end/lambda.js
+++
@@ -1,7 +0,0 @@
-const serverlessExpress = require('aws-serverless-express');
-const app = require('./index_servidor'); // Importa o app configurado
-const server = serverlessExpress.createServer(app);
-
-exports.handler = (event, context) => {
-    return serverlessExpress.proxy(server, event, context);
-};
